3741dc3f57c22ddf2468fea44b8f79d9b9836adf
grayrat <grayrat1999@163.com>
2026-01-18 16:10:18 +0800

    opt: 优化逻辑

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from stock_zh import listStockInfoCN
 from image_export import export
 
 years = 1
-listStockInfoCN(symbol='159525', days=years * 365)
+listStockInfoCN(symbol='512890', days=years * 365)
 listStockInfoUS('.NDX')
 listStockInfoUS('.INX')
 
-export(source_file_name='159525_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+export(source_file_name='512890_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
 export(source_file_name='.INX_近年数据.csv', buy_rate = - 0.1, sell_rate = 0.15, years=years)
 export(source_file_name='.NDX_近年数据.csv', buy_rate = - 0.1, sell_rate = 0.15, years=years)
