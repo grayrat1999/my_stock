21027bf81c540cdee51ad755103631a919fbb632
grayrat <grayrat1999@163.com>
2026-01-31 13:24:38 +0800

    opt: 优化逻辑

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from stock_us import listStockInfoUS
-from stock_zh import listStockInfoCN
+from stock_zh import listIndexInfoCN, listETFInfoCN
 from image_export import export
 
 years = 1
-listStockInfoCN(symbol='512890', days=years * 365)
-listStockInfoUS('.NDX')
-listStockInfoUS('.INX')
 
-export(source_file_name='512890_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+# 港股低波红利ETF摩根
+listETFInfoCN(symbol='513630', days=years * 365)
+export(source_file_name='513630_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+
+# 红利ETF易方达
+listETFInfoCN(symbol='515180', days=years * 365)
+export(source_file_name='515180_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+
+# 医药ETF
+listETFInfoCN(symbol='159929', days=years * 365)
+export(source_file_name='159929_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+
+# 恒生科技ETF
+listETFInfoCN(symbol='513010', days=years * 365)
+export(source_file_name='513010_近年数据.csv', buy_rate = - 0.07, sell_rate = 0.07, years=years)
+
+# 标普
+listStockInfoUS('.INX')
 export(source_file_name='.INX_近年数据.csv', buy_rate = - 0.1, sell_rate = 0.15, years=years)
+
+# 纳指
+listStockInfoUS('.NDX')
 export(source_file_name='.NDX_近年数据.csv', buy_rate = - 0.1, sell_rate = 0.15, years=years)
--- stock_zh.py
+++ stock_zh.py
@@ -3,7 +3,7 @@ import akshare as ak
 from datetime import datetime, timedelta
 from stock_const import data_dir 
 
-def listStockInfoCN(symbol, days=365):
+def listIndexInfoCN(symbol, days=365):
     end_dt = datetime.today()
     start_dt = end_dt - timedelta(days)
     # 获取历史指数日线数据
@@ -20,3 +20,25 @@ def listStockInfoCN(symbol, days=365):
     df.columns = ["date", "open", "close"]
     df = df.sort_values("date")
     df.to_csv(f"{data_dir}/{symbol}_近年数据.csv", index=False, encoding="utf-8-sig")
+
+
+def listETFInfoCN(symbol, days=365):
+    end_dt = datetime.today()
+    start_dt = end_dt - timedelta(days)
+
+    # 获取 ETF 日线行情（东方财富）
+    df = ak.fund_etf_hist_em(
+        symbol=symbol,
+        period="daily",
+        start_date=start_dt.strftime("%Y%m%d"),
+        end_date=end_dt.strftime("%Y%m%d"),
+        adjust=""
+    )
+    df = df[["日期", "开盘", "收盘"]]
+    df.columns = ["date", "open", "close"]
+    df = df.sort_values("date")
+    df.to_csv(
+        f"{data_dir}/{symbol}_近年数据.csv",
+        index=False,
+        encoding="utf-8-sig"
+    )
