665c30cd9d234254c5ab09eb6456e44d5b500e91
grayrat <grayrat1999@163.com>
2026-02-03 18:02:28 +0800

    opt: 优化逻辑

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def queryIndexUs(params):
         export(source_file_name=f'{param.symbol}_近年数据.csv', buy_rate=param.buy_rate, sell_rate=param.sell_rate, years=years)
 
 queryETF([
-        # 港股低波红利ETF摩根
-        QueryParam(symbol='513630', buy_rate=-0.07, sell_rate=0.07),
-        # 红利ETF易方达
-        QueryParam(symbol='515180', buy_rate=-0.07, sell_rate=0.07),
         # 医药ETF
         QueryParam(symbol='159929', buy_rate=-0.07, sell_rate=0.07),
         # 恒生科技ETF
         QueryParam(symbol='513010', buy_rate=-0.07, sell_rate=0.07),
         # 消费ETF
         QueryParam(symbol='159798', buy_rate=-0.07, sell_rate=0.07),
+        # 港股低波红利ETF摩根
+        QueryParam(symbol='513630', buy_rate=-0.07, sell_rate=0.07),
+        # 红利ETF易方达
+        QueryParam(symbol='515180', buy_rate=-0.07, sell_rate=0.07),
 ])
 
 queryIndexUs([
